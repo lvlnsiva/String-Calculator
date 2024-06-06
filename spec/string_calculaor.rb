1d7063ae82a313ec6813fa0e37dbedba15f857aa
lvlnsiva <sivaprasad.n@bitlasoft.com>
2024-06-06 08:38:13 +0530

    Initial spec commit

---
+++ spec/string_calculaor.rb
@@ -0,0 +1,12 @@
+require 'string_calculator'
+
+# The top of your testing block
+# More examples of tests can be found here: https://github.com/rspec/rspec-expectations
+RSpec.describe StringCalculator, "#add" do
+
+  it "returns 0 for empty string" do
+    expect(StringCalculator.add("")).to eql(0)
+  end
+
+  # more tests go here
+end
